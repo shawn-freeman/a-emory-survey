94213f57a1a4d14187d8f3139a194be98b0f7cb2
Shawn Freeman <magus_zsf@hotmail.com>
2022-03-24 15:49:05 -0400

    Seeded default question definitions

--- a-emory-survery/a-emory-survery-api/Dal/EmorySurveyDbContext.cs
+++ a-emory-survery/a-emory-survery-api/Dal/EmorySurveyDbContext.cs
@@ -10,6 +10,17 @@ namespace a_emory_survery_api.Dal
         }
 
         public DbSet<QuestionDefinition> QuestionDefinition { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<QuestionDefinition>().HasData(
+                new QuestionDefinition() { Id = 1, Description = "Question 1" },
+                new QuestionDefinition() { Id = 2, Description = "Question 2" },
+                new QuestionDefinition() { Id = 3, Description = "Question 3" },
+                new QuestionDefinition() { Id = 4, Description = "Question 4" },
+                new QuestionDefinition() { Id = 5, Description = "Question 5" }
+                );
+        }
     }
 
     
---
+++ a-emory-survery/a-emory-survery-api/Migrations/20220324194731_seedQuestions.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace a_emory_survery_api.Migrations
+{
+    public partial class seedQuestions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "QuestionDefinition",
+                columns: new[] { "Id", "Description" },
+                values: new object[,]
+                {
+                    { 1, "Question 1" },
+                    { 2, "Question 2" },
+                    { 3, "Question 3" },
+                    { 4, "Question 4" },
+                    { 5, "Question 5" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "QuestionDefinition",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "QuestionDefinition",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "QuestionDefinition",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "QuestionDefinition",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "QuestionDefinition",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
