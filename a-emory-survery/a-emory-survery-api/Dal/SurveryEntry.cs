a9fb277a29de20e42243fe2f9862ad9a42f1f7c6
Shawn Freeman <magus_zsf@hotmail.com>
2022-03-24 16:05:36 -0400

    Added SurveyEntry and SurveyQuestion models

---
+++ a-emory-survery/a-emory-survery-api/Dal/SurveryEntry.cs
@@ -0,0 +1,9 @@
+﻿namespace a_emory_survery_api.Dal
+{
+    public class SurveryEntry
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string VerificationCode { get; set; }
+    }
+}
---
+++ a-emory-survery/a-emory-survery-api/Dal/SurveyQuestion.cs
@@ -0,0 +1,11 @@
+﻿namespace a_emory_survery_api.Dal
+{
+    public class SurveyQuestion
+    {
+        public int Id { get; set; }
+        public int SurveyEntryId { get; set; }
+        public int QuestionDefId { get; set; }
+
+        public string Answer { get; set; }
+    }
+}
