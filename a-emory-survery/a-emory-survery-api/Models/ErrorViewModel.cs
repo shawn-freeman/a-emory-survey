207f8568f8ab35d51dde2c063a81262fdad385ef
Shawn Freeman <magus_zsf@hotmail.com>
2022-03-24 15:05:43 -0400

    initial core 6 project commit

---
+++ a-emory-survery/a-emory-survery-api/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace a_emory_survery_api.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
