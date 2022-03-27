9f021b405c9d7b68e83dd9d9c366d534015d74a4
Shawn Freeman <magus_zsf@hotmail.com>
2022-03-27 13:24:25 -0400

    Adding verification code generation

--- a-emory-survery/a-emory-survery-api/Controllers/EmailVerificationController.cs
+++ a-emory-survery/a-emory-survery-api/Controllers/EmailVerificationController.cs
@@ -1,19 +1,39 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Globalization;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace a_emory_survery_api.Controllers
 {
     public class EmailVerificationController : Controller
     {
+        private const int CODE_LENGTH = 5;
+        private EmorySurveyDbContext _dbContext;
+        public EmailVerificationController(EmorySurveyDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
         public ActionResult ValidateEmail(string email)
         {
             var isValidEmail = IsValidEmail(email);
 
             if (isValidEmail)
             {
-                //TODO: Send an actual email to confirm
+                //check if one exists
+                var entry = _dbContext.SurveyEntry.FirstOrDefault(se => se.Email == email);
+
+                if (entry == null)
+                {
+                    var code = GenerateVerificationCode(CODE_LENGTH);
+                    _dbContext.SurveyEntry.Add(new SurveyEntry() { Email = email, VerificationCode = code });
+                    _dbContext.SaveChanges();
+                }
+                else
+                {
+
+                }
                 return Ok();
             }
             else
@@ -78,6 +98,21 @@ namespace a_emory_survery_api.Controllers
             {
                 return false;
             }
+        }
+
+        private string GenerateVerificationCode(int numDigits)
+        {
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+            Random rnd = new Random();
+            StringBuilder b = new StringBuilder(numDigits);
+            for (int i = 0; i < numDigits; i++)
+            {
+                b.Append(chars[rnd.Next(chars.Length)]);
+            }
+            string result = b.ToString();
+
+            return result;
         }
     }
 }
