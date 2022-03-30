47fc9d2d7b412c1501aa6765707996dc57d13141
Shawn Freeman <magus_zsf@hotmail.com>
2022-03-30 13:30:37 -0400

    Initial about survey description

--- a-emory-react-native/components/about-survey-component.js
+++ a-emory-react-native/components/about-survey-component.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableOpacity, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Dimensions, TouchableOpacity, StyleSheet, Text, TextInput, View } from 'react-native';
 import { Pages } from '../constants/pages';
 import { appStyles } from '../constants/styles';
 
@@ -8,7 +8,19 @@ export class AboutSurveyComponent extends React.Component
     render(){
     return (
         <View style={appStyles.container}>
-        <Text>About A Emory Survey</Text>
+         <View style={appStyles.aboutTextcontainer} >
+            <Text>Emory is a variant of the English name Emery. The origin of this name is Germanic, from the Germanic personal name 'Amalric'. The name 'Amalric' is made of two parts, 'amal' meaning 'courageous' and 'ric' meaning 'powerful. Emory means 'a powerfully courageous person'.</Text>
+
+            <h3>So what is <strong>A Emory Survey</strong>?</h3>
+
+            <Text>Part of being a 'courageous person' or 'powerful leader' is the ability 
+               to ask and answer the most difficult questions our society faces. 
+               <strong> A Emory Survey</strong> provides an annoymous method to answering 
+               such questions free of judgement. Participation in the survey is completely 
+               confidencial, voluntarily and is not shared with any third-party.</Text>
+         </View>
+
+        
 
         <View style={localStyles.buttonContainer} >
 
@@ -28,6 +40,8 @@ export class AboutSurveyComponent extends React.Component
 }
 
 const localStyles = StyleSheet.create({
+   aboutTextcontainer: {
+   },
    buttonContainer: {
       flexDirection: 'row',
    },
