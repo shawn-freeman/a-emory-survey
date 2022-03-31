0aec355d873467a4d9a317e181d4c557b94dcb5f
Shawn Freeman <magus_zsf@hotmail.com>
2022-03-31 12:57:24 -0400

    Updating seeded questions

--- a-emory-survery/a-emory-survery-api/Migrations/20220324194731_seedQuestions.cs
+++ a-emory-survery/a-emory-survery-api/Migrations/20220324194731_seedQuestions.cs
@@ -13,11 +13,13 @@ namespace a_emory_survey_api.Migrations
                 columns: new[] { "Id", "Description" },
                 values: new object[,]
                 {
-                    { 1, "Question 1" },
-                    { 2, "Question 2" },
-                    { 3, "Question 3" },
-                    { 4, "Question 4" },
-                    { 5, "Question 5" }
+                    { 1, "Do you consider yourself a part of the LGTBQ community?" },
+                    { 2, "What is your blood type?" },
+                    { 3, "If you worked in a software position for 5 years using a technology stack that included but not limited to C# .NET, .NET Core, SQL Server, Angular, React, Javascript, HTML, CSS, Xamarin, SOAP, UWP, EntityFramework, MVC, WPF, CosmosDB, Node.JS, Unity3d, OpenSSL and Microsoft Azure how much experience would you say you had in each?" },
+                    { 4, "You are a member of the LGTBQ community and someone does not understand your way of life. What would be your drug of choice to punish the individual?" },
+                    { 5, "If someone were to say to you \"I am Poly.\" how would you interpret the statement?" },
+                    { 6, "If someone demonstrated romantic interest in you, how might you abuse this person?" },
+                    { 7, "In your own words, describe love." },
                 });
         }
 
