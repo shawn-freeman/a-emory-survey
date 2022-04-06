14b5d8d5ada93a0b87eb4b7f3a3833b0ed390262
Shawn Freeman <magus_zsf@hotmail.com>
2022-04-06 11:19:09 -0400

    Options pattern for RDS options

---
+++ a-emory-survery/a-emory-survery-api/Models/RDSOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace a_emory_survery_api.Models
+{
+    public class RDSOptions
+    {
+        public string Hostname { get; set; }
+        public string Port { get; set; }
+        public string DbName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
